dfe4b89390ad3828d43119a8d2cd53d88b448f3f
Kyle1668 <kyledevinobrien1@gmail.com>
2017-07-09 22:34:03 -0700

    Insertion Sort

---
+++ Algorithms/Insertion Sort/src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    static int[] insertionSort(int[] inNums) {
+        int temp;
+        int position;
+        boolean inserted;
+        for (int i = 1; i < inNums.length; i++) {
+            if (inNums[i] < inNums[i - 1]) {
+                position = i;
+                inserted = false;
+                while (!inserted) {
+                    if (position == 0 || inNums[position] >= inNums[position - 1]) {
+                        inserted = true;
+                    }
+                    else {
+                        temp = inNums[position - 1];
+                        inNums[position - 1] = inNums[position];
+                        inNums[position] = temp;
+                        position--;
+                    }
+                }
+            }
+        }
+        return inNums;
+    }
+
+    public static void main(String[] args) {
+
+        int[] intArray1 = {46,14,91,31,84,48,35,95,39,54,10,42,79,61,62,50,46,4,80,73};
+        intArray1 = insertionSort(intArray1);
+
+        for (int element: intArray1) {
+            System.out.print(element + " ");
+        }
+
+    }
+}
